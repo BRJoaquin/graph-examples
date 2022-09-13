e2c556027c953052ab9f47d9fe1f0b5ef97cc6a5
Joaquin Vigna <joaquin@brokenrubik.co>
2022-09-13 11:56:39 -0300

    adding intersection observer

--- pages/nested/main.ts
+++ pages/nested/main.ts
@@ -30,29 +30,44 @@ const gData = {
   nodes: [...Array(V).keys()].map((i) => ({ id: i, selected: 1 })),
   links
 };
-console.log(gData);
 
 
-const distance = 400;
+const target = document.getElementById('3d-graph');
 
-const Graph = ForceGraph3D()(document.getElementById("3d-graph")!)
-  .graphData(gData)
-  .linkCurvature(0)
-  .linkWidth(1.5)
-  .enableNodeDrag(false)
-  .enableNavigationControls(false)
-  .showNavInfo(false)
-  .cameraPosition({ z: distance })
-  .backgroundColor("#121212")
-  
-// distance beteween link reperesent the cost to go
-// Graph.d3Force('link')?.distance((link : any )=> link.value);
+let loaded = false;
 
-let angle = 0;
-setInterval(() => {
-  Graph.cameraPosition({
-    x: distance * Math.sin(angle),
-    z: distance * Math.cos(angle)
+function handleIntersection(entries : IntersectionObserverEntry[]) {
+  entries.map((entry) => {
+    if (entry.isIntersecting && !loaded) {
+      
+      const distance = 400;
+
+      const Graph = ForceGraph3D()(document.getElementById("3d-graph")!)
+        .graphData(gData)
+        .linkCurvature(0)
+        .linkWidth(1.5)
+        .enableNodeDrag(false)
+        .enableNavigationControls(false)
+        .showNavInfo(false)
+        .cameraPosition({ z: distance })
+        .backgroundColor("#121212")
+        
+      // distance beteween link reperesent the cost to go
+      // Graph.d3Force('link')?.distance((link : any )=> link.value);
+
+      let angle = 0;
+      setInterval(() => {
+        Graph.cameraPosition({
+          x: distance * Math.sin(angle),
+          z: distance * Math.cos(angle)
+        });
+        angle += Math.PI / 1000;
+      }, 10);
+
+      loaded = true;
+    }
   });
-  angle += Math.PI / 1000;
-}, 10);
+}
+
+const observer = new IntersectionObserver(handleIntersection);
+observer.observe(target!);
